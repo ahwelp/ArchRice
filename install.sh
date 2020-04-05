dfac6c40b80724835bb607f1bf3952c368435fdb
Artur Welp <ahwelp@universo.univates.br>
2020-04-05 11:36:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@
     bootctl install
     uuid=`cat /etc/fstab | grep ext4 | grep '/' | cut -d $'\t' -f1 | cut -d '=' -f 2`
     partuuid=`blkid | grep $uuid | cut -d' ' -f 7 | sed 's/\"//g'`
-    printf "default arch\ntimeout 2" >> /boot/loader/loader.conf
-    printf "title ArchLinux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=$partuuid rw" >> /boot/loader/entries/arch.conf
+    printf "default arch\ntimeout 2" > /boot/loader/loader.conf
+    printf "title ArchLinux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=$partuuid rw" > /boot/loader/entries/arch.conf
   else
     echo "There is a bootloader"
   fi
