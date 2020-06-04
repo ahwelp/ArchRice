7eb310e5ee3f3df9c0d92df6307e3b9803634a60
ahwelp <ahwelp@test.com>
2020-06-04 19:02:26 -0300

    Descriptions and the filename cant have foward slash char

--- scripts/ytdlPart.sh
+++ scripts/ytdlPart.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Usage: ytdlPart https://www.youtube.com/watch?v=xxxxxxxxx 00:10:00 00:01:00 video.mp4
+# Download 1 minute from the video, starting from 00:10:00. So from 00:10:00 to 00:11:00
+
 MESSAGE=''
 
 #Check the stuff
-  [ "$1" == "" ] && ! MESSAGE="$MESSAGE First argument must be a video URL\n"                         #Needs a youtube link 
-  [ "$2" == "" ] && ! MESSAGE="$MESSAGE Second argument must be a time start 00:00:00\n"              #Needs a time start
-  [ "$3" == "" ] && ! MESSAGE="$MESSAGE Third argument must be the length from the point 00:00:00\n"  #Needs how many time to extract
+  #Needs a youtube link 
+  [ "$1" == "" ] && ! MESSAGE="$MESSAGE First argument must be a video URL\n"
+  #Needs a time start
+  [ "$2" == "" ] && ! MESSAGE="$MESSAGE Second argument must be a time start 00:00:00\n"
+  #Needs how many time to extract
+  [ "$3" == "" ] && ! MESSAGE="$MESSAGE Third argument must be the length from the point 00:00:00\n"
 
 #printf "$MESSAGE"
 
@@ -20,8 +26,8 @@ MESSAGE=''
 
 #Handle filename
   FILENAME=$4
-  [ "$4" == "" ] && ! FILENAME=`youtube-dl -e $1`".mp4" #If no name defined, get the video name
-  FILENAME=`echo $FILENAME | sed s/\ /_/g`        #No spaces on the filename
+  [ "$4" == "" ] && ! FILENAME=`youtube-dl -e $1`".mkv"      #If no name defined, get the video name
+  FILENAME=`echo $FILENAME | sed s/\ /_/g | sed "s/\//-/g"`  #No spaces or / on the filename
 
 #Do the download
   ffmpeg -ss $2 -i "$FIRST" -t $3 -ss $2 -i "$SECOND" -t $3 -c copy -c:a aac $FILENAME
