124b9a8c626edfd5ac22b6182507264917129bc5
Artur Welp <ahwelp@universo.univates.br>
2025-11-27 04:10:23 -0300

    More changes

--- legacy.sh
+++ legacy.sh
@@ -1,57 +1,81 @@
 #!/bin/bash
 set -e
 
-# Core info
+## ToDo
+## Add user to groups to sudo
+
+###############################################################################
+# Core info ###################################################################
 DISK="/dev/sda"
-HOST_NAME="<HOST_NAME>" #ArchServer
-ROOT_PASSWORD="<SECRET>" # openssl rand -base64 16
+HOST_NAME="ArchServer" # ArchServer
+ROOT_PASSWORD=`openssl rand -base64 16` # Root Password
 
-# User info
-USER="<USERNAME>"
-USER_PASS="<SECRET>" # openssl rand -base64 16
+###############################################################################
+# User info ###################################################################
+USER="<username>"
+USER_PASS=`openssl rand -base64 16`
 
-# Location info
-LOCALE_1="<LOCALE_1>"  # pt_BR.UTF-8 UTF-8
-LOCALE_2="<LOCALE_2>"   # pt_BR ISO-8859-1
-TIME_ZONE="<TIME_ZONE>" # America/Sao_Paulo
+###############################################################################
+# Location info ###############################################################
+LOCALE_1="<LOCALE_1>" # pt_BR.UTF-8
+LOCALE_2="<LOCALE_2>" # pt_BR.ISO-8859-1
+TIME_ZONE="<TIMEZONE>" # America/Sao_Paulo
 
-log(){ echo "============================"; echo $1; sleep 2; }    
+###############################################################################
+echo "Setting up Machine" ######################################################
+SILENT_LOG="$(mktemp)"
+log(){ echo "============================"; echo $1; }
+logt(){ echo "============================"; echo $1; sleep 2; }
 fuck(){ swapoff /dev/sda1 2>/dev/null || true; umount -R /mnt 2>/dev/null || true; }
+silence(){ exec 3>&1 4>&2; exec 1>"$SILENT_LOG" 2>&1; }
+voice(){ exec 1>&3 2>&4; exec 3>&- 4>&-; }
+show_buffer(){ cat "$SILENT_LOG"; }
+clear_buffer(){ : > "$SILENT_LOG"; }
 trap 'fuck $?' ERR
 
-# Some setup
+###############################################################################
+log "Setting up host machine" #################################################
+silence
 timedatectl set-ntp true
 pacman -Sy archlinux-keyring --noconfirm
 pacman -Syy --noconfirm
+voice
+logt "Host Machine is Configured" #############################################
 
-# Disk geometry
+###############################################################################
+log "Creating Disk Gemometry" #################################################
+silence
 wipefs -a $DISK
 printf "o\nn\np\n1\n\n+4G\nt\n82\nn\np\n2\n\n\nw\n" | fdisk $DISK
+voice
+logt "Geometry done" ##########################################################
 
-#Swap
-mkswap ${DISK}1
-swapon ${DISK}1
+###############################################################################
+log "Formating Partitions" ####################################################
+silence
+mkswap ${DISK}1 #Swap
+swapon ${DISK}1 #Swap
+mkfs.ext4 -F ${DISK}2 # Disk File Systems
+voice
+log "Disk configuration Done" #################################################
 
-# Disk File Systems
-mkfs.ext4 -F ${DISK}2
-
-log "Disk configuration Done"
-
-#Install
+###############################################################################
+log "Starting Installation" ###################################################
 mount --mkdir ${DISK}2 /mnt
-
-pacstrap -K /mnt base base-devel linux linux-firmware man curl grub os-prober networkmanager qemu-guest-agent ntfs-3g vim git 
+PACKAGES="base base-devel linux linux-firmware grub man curl os-prober iptables-nft"
+PACKAGES="$PACKAGES networkmanager qemu-guest-agent ntfs-3g vim git openssh"
+pacstrap -K /mnt $PACKAGES
 genfstab -U /mnt >> /mnt/etc/fstab
+logt "Instalation complete" ###################################################
 
-log "Instalation complete"
-
-# Installing Bootloader
+###############################################################################
+log "Installing Bootloader" ###################################################
 arch-chroot /mnt /bin/bash -c "grub-install --target=i386-pc $DISK"
 arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
+logt "Boot Loader installed" #################################################
 
-log "Boot Loader installed"
-
-# Manage Users
+###############################################################################
+log "Setting up user" #########################################################
 # ToDo add the user to sudoers. It's not working with wheel
 arch-chroot /mnt /bin/bash -c "echo root:$ROOT_PASSWORD | chpasswd"
 arch-chroot /mnt /bin/bash -c "groupadd -f wheel && groupadd -f sudo"
@@ -59,10 +83,10 @@ arch-chroot /mnt /bin/bash -c "useradd -m -s /bin/bash $USER"
 arch-chroot /mnt /bin/bash -c "sed -i \"s/^wheel:x:[0-9]\+:/wheel:x:998:${USER}/\" /etc/group"
 arch-chroot /mnt /bin/bash -c "echo $USER:$USER_PASS | chpasswd"
 arch-chroot /mnt /bin/bash -c "sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers"
+logt "User configuration done" ################################################
 
-log "User configuration done"
-
-# Locale And Timezone
+###############################################################################
+log "Locale And Timezone" #####################################################
 arch-chroot /mnt /bin/bash -c "ln -s /usr/share/zoneinfo/$TIME_ZONE /etc/localtime"
 arch-chroot /mnt /bin/bash -c "echo $LOCALE_1 >> /etc/locale.gen"
 arch-chroot /mnt /bin/bash -c "echo $LOCALE_2 >> /etc/locale.gen"
@@ -71,35 +95,26 @@ arch-chroot /mnt /bin/bash -c "localectl set-locale LANG=$LOCALE_1"
 arch-chroot /mnt /bin/bash -c "sudo -u $USER localectl set-locale LANG=$LOCALE_1"
 arch-chroot /mnt /bin/bash -c "locale-gen"
 arch-chroot /mnt /bin/bash -c "sudo -u $USER locale-gen"
+logt "Time Zone and location done" ############################################
 
-log "Time Zone and location done"
-
-# Network
+###############################################################################
+log "Setting Up Network" ######################################################
 arch-chroot /mnt /bin/bash -c "systemctl enable NetworkManager"
 arch-chroot /mnt /bin/bash -c "echo $HOST_NAME > /etc/hostname"
+logt "NetworkManager config done" #############################################
 
-log "NetworkManager config done"
-
-# Branding for style
+###############################################################################
+log "Branding for style" ######################################################
 arch-chroot /mnt /bin/bash -c "curl https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch > /usr/local/bin/neofetch"
 arch-chroot /mnt /bin/bash -c "chmod +x /usr/local/bin/neofetch"
 arch-chroot /mnt /bin/bash -c "neofetch > /etc/issue && echo \"(\l)\" >> /etc/issue"
+logt "Style is done " #########################################################
 
-log "Style is done "
-
-# Install YAY
-arch-chroot /mnt /bin/bash -c "git clone https://aur.archlinux.org/yay-bin.git /usr/src/yay-bin"
-arch-chroot /mnt /bin/bash -c "pushd /usr/src/yay-bin"
-arch-chroot /mnt /bin/bash -c "git config --add core.filemode false"
-arch-chroot /mnt /bin/bash -c "sudo -u $USER makepkg -si --noconfirm"
-arch-chroot /mnt /bin/bash -c "popd"
-
-log "Yay installation is done"
-
-# Start other services
+###############################################################################
+log "Start other services" ####################################################
+arch-chroot /mnt /bin/bash -c "systemctl enable sshd"
 arch-chroot /mnt /bin/bash -c "systemctl enable qemu-guest-agent"
+log "Other services enabled" ##################################################
 
-log "Other services enabled"
-
-log "Rebooting"
-reboot
+#log "Rebooting"
+#reboot
